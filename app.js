944741ba86d3e514e512d629ae0ea2ac21b4c8d4
Bjorn Pardo <bjorn@xend.com.ph>
2014-05-06 17:05:36 -0600

    done with bonus

--- app.js
+++ app.js
@@ -2,11 +2,38 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 
-var fileContents = fs.readFileSync('data.txt');
+//------------- Part 2
+// var fileContents = fs.readFileSync('data3.txt');
 
-app.get('/', function(req, res) {
-	res.header('Content-Type', 'text/html');
-	res.send(fileContents);
+// app.get('/', function(req, res) {
+// 	res.header('Content-Type', 'text/html');
+// 	res.send(fileContents);
+// });
+
+// var server = app.listen(3587, function() {
+// 	console.log('Express server listening on port ' + server.address().port);
+// });
+
+
+//-------------- Part 3
+// app.get('/', function(req, res) {
+// 	fs.readFile('data.txt', function(err, data){
+// 		res.header('Content-Type', 'text/html');
+// 		res.send(data);
+// 	});	
+// });
+
+// var server = app.listen(3587, function() {
+// 	console.log('Express server listening on port ' + server.address().port);
+// });
+
+
+//---------------- Bonus
+app.get('/:filename', function(req, res) {
+	fs.readFile('./public/' + req.params.filename, function(err, data){
+		res.header('Content-Type', 'text/html');
+		res.send(data);
+	});	
 });
 
 var server = app.listen(3587, function() {
